c6e9adeaf4994abcb3279c46b95fd1e4101dcb08
NPXQuinn <quinn.hodges@npxinnovation.ca>
2020-05-22 19:59:07 -0400

    Updates

---
+++ precache-manifest.09db8132e76ffbe9eef10f651fb4b6f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e3de33521eebeb850394aa7e00ba83",
+    "url": "/recipe-book/index.html"
+  },
+  {
+    "revision": "1775ef7870ddf6139d69",
+    "url": "/recipe-book/static/js/2.636085c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-book/static/js/2.636085c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3257b62645124df503ee",
+    "url": "/recipe-book/static/js/main.eaae4828.chunk.js"
+  },
+  {
+    "revision": "e43dee66e22b672b1fce",
+    "url": "/recipe-book/static/js/runtime-main.e7a7df35.js"
+  }
+]);
